c71bd044885999eb62e301fcf6229bcb8d08573a
Conor <220956160+conor-sm@users.noreply.github.com>
2026-01-17 13:07:08 +0000

    Added ClickBox and Visuals Class. They both control managing user clicks (I updated clicking logic) and blitting click count status and a placeholder image for a logo. I introduced a level system, with level 1 at 100 clicks and level 2 at 500 clicks, they each change the image of the click box. Cleaned up some areas. Lot of time debugging. - Conor Smallwood (roughly) 1 hour 20 minutes

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import asyncio
 
 pygame.init()
 
+IMAGE_L1 = pygame.transform.scale(pygame.image.load("data/image-1.png"), (125, 83))
+IMAGE_L2 = pygame.transform.scale(pygame.image.load("data/image-2.png"), (125, 83))
+IMAGE_L3 = pygame.transform.scale(pygame.image.load("data/image-3.png"), (125, 83))
+LOGO = pygame.transform.scale(pygame.image.load("data/image-4.png"), (150, 80))
+
 class Game():
     def __init__(self):
         self.running = True
@@ -13,7 +18,12 @@ class Game():
         self.screen = pygame.display.set_mode((900, 400))
 
     def game_loop(self):
-        self.rect = pygame.draw.rect(self.screen, (255, 0, 0),(pygame.Rect(125, 83, (900 + 125) // 2, ((400 + 83) // 2))))
+        self.screen.fill((0, 0, 0))
+
+        box.update()
+        box.draw()
+        vis.draw()
+
         pygame.display.update()
         self.clock.tick(60)
 
@@ -21,21 +31,62 @@ class Game():
         pygame.display.update()
         self.clock.tick(60)
 
-#class ClickBox():
- #   def __init__(self):
+class ClickBox():
+    def __init__(self):
+        self.image = IMAGE_L1
+        self.image_rect = self.image.get_rect(center=(450, 200))
 
+        self.level_state = None
+
+        self.counter = 0
+
+    def level_1(self):
+        self.image = IMAGE_L2
+        self.image_rect = self.image.get_rect(center=self.image_rect.center)
+
+    def level_2(self):
+        self.image = IMAGE_L3
+        self.image_rect = self.image.get_rect(center=self.image_rect.center)
+
+    def update(self):
+        if self.counter >= 100:
+            self.level_state = "level_one"
+            box.level_1()
+
+        if self.counter >= 500:
+            self.level_state = "level_two"
+            box.level_2()
+
+    def draw(self):
+        game.screen.blit(self.image, self.image_rect)
+
+class Visuals:
+    def __init__(self):
+        self.large_font = pygame.font.SysFont("Arial", 32, False)
+        self.small_font = pygame.font.SysFont("Arial", 18, False)
+        self.logo_image = LOGO
+        self.colour = (255, 0, 0)
+    
+    def draw(self):
+        self.score_txt = self.large_font.render(f"Clicks: {box.counter}", True, self.colour)
+        game.screen.blit(self.score_txt, ((900 - self.score_txt.get_width()) // 2, 100))
+        game.screen.blit(self.logo_image, ((900 - 150) // 2, 0))
+game = Game()
+box = ClickBox()
+vis = Visuals()
 
 async def main():
-    game = Game()
-    #box = ClickBox()
     while game.running:
         mouse_position = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.running = False
             
-            if event.type == pygame.MOUSEBUTTONDOWN and game.rect.collidepoint(mouse_position):
-                print("PLANE CLICKED")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if box.image_rect.collidepoint(mouse_position):
+                        box.counter += 1
+                        print(f"PLANE CLICKED ||| {box.counter}")
 
         if game.game_active:
             game.game_loop()
